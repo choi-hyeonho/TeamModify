314b601d33120aac391f7cf4c2a2c3c0a6bea112
Choi hyeon ho <chh-95@hanmail.net>
2023-07-27 00:38:31 +0900

    날아간 코드 원상복구

--- Web/Final_team_project_1/team1/app.py
+++ Web/Final_team_project_1/team1/app.py
@@ -151,28 +151,22 @@ def predict():
     Steel_Plate_Thickness = request.form['Steel_Plate_Thickness']
     Empty_Index = request.form['Empty_Index']
     Type_of_Steel = request.form['Type_of_Steel']
-
     # 입력값을 데이터에 맞게 변환
     if Type_of_Steel == 'A300':
         Type_of_Steel = 0
     else:
         Type_of_Steel = 1
-
     # 개별 데이터를 NumPy 배열로 표현
     individual_data = np.array([X_Minimum, X_Maximum, Y_Minimum, Y_Maximum, Pixels_Areas,
                                 X_Perimeter, Y_Perimeter, Sum_of_Luminosity, Minimum_of_Luminosity,
                                 Maximum_of_Luminosity, Length_of_Conveyer, Steel_Plate_Thickness,
                                 Empty_Index, Type_of_Steel], dtype=object)
-
     data_array = individual_data.reshape(1, -1)  # 개별 데이터를 2차원 배열로 변환
     data_array = np.array(data_array).astype(float)  # str 형태의 데이터를 flaot으로 변환
-
     # 예측 수행
     prediction1 = model1.predict(data_array)
-
     # 타겟 리스트
     target_list = ['Pastry', 'Z_Scratch', 'K_Scatch', 'Stains', 'Dirtiness', 'Bumps', 'Other_Faults']
-
     # 결과 반환
     if prediction1.tolist()[0] == 5 or prediction1.tolist()[0] == 6:
         data_array2 = np.delete(data_array, -2, axis=1)  # 'Empty Index' 데이터 삭제
@@ -182,7 +176,6 @@ def predict():
     else:
         num = prediction1.tolist()[0]
         result = target_list[num]
-
     return jsonify({'prediction_result': result})
 '''
 # 긴 결과값이 나으는 문제 해결을 위해 predict() 클래스 변경
