7069199489f9a1508a76809afd3b1be6cada6b27
HeeYoung-code <octoberhee0@gmail.com>
2023-07-27 15:00:27 +0900

    0727_update

--- Web/Final_team_project_1/team1/app.py
+++ Web/Final_team_project_1/team1/app.py
@@ -2,12 +2,28 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 import joblib
 import os
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 import pickle
 import json
 
 app = Flask(__name__)
 
-# 모델 로드
+
+# pulsar star 스케일러/모델 로드
+pulsar_scaler_path = os.path.join(os.path.dirname(__file__), 'pulsar_scaler.pkl')
+pulsar_scaler = joblib.load(pulsar_scaler_path)
+
+def load_keras_model():
+    tf.keras.backend.clear_session() # Clear the session and optimizer state to avoid M1/M2 Mac optimizer warning
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(current_dir, 'pulsar_model.h5')
+    model = load_model(model_path)
+    return model
+pulsar_model = load_keras_model()
+
+
+# steel plate 모델 로드
 model_path1 = os.path.join(os.path.dirname(__file__), 'steelplate_model1.pkl')
 model1 = joblib.load(model_path1)
 
@@ -18,6 +34,56 @@ model2 = joblib.load(model_path2)
 age_prediction_model = None
 #crack_classification_model = None
 mactility_discrimination_model = None
+
+
+
+
+
+
+################## pulsar 처리 함수
+def process_input_data_pulsar(request):
+    
+    # key에 매치하는 정보 불러오기
+    Mean_profile = request.form['MeanProfileSlider']
+    Standard_profile = request.form['StandardProfileSlider']
+    Excess_profile = request.form['ExcessProfileSlider']
+    Skewness_profile = request.form['SkewnessProfileSlider']
+    Mean_DM = request.form['MeanDMSlider']
+    Standard_DM = request.form['StandardDMSlider']
+    Excess_DM = request.form['ExcessDMSlider']
+    Skewness_DM = request.form['SkewnessDMSlider']
+
+    '''
+    input_str = input()
+    input_data = np.array(input_str.split(), dtype=float)
+
+    # Transform the input data using the trained scaler
+    input_scaled = scaler.transform(input_data)
+    '''
+
+    # 개별 데이터를 NumPy 배열로 표현
+    individual_data = np.array([Mean_profile, Standard_profile, Excess_profile, Skewness_profile,
+                                Mean_DM, Standard_DM, Excess_DM, Skewness_DM], dtype=float)
+    data_array = individual_data.reshape(1, -1)  # 개별 데이터를 2차원 배열로 변환
+
+    data_array = pulsar_scaler.transform(data_array) # scaling
+
+    return data_array
+
+
+def process_pulsar_input():
+    data_array = process_input_data_pulsar(request)
+
+    # 예측 수행
+    prediction = (pulsar_model.predict(data_array) > 0.5).astype(int).flatten()
+
+    if prediction[0] == 1:
+        return "축하합니다! 맥동성을 찾으셨습니다"
+    else:
+        return "아쉽게도 맥동성이 아닙니다"
+
+
+
 
 
 # 강판 불량 입력 데이터를 처리하는 함수
@@ -76,6 +142,9 @@ def process_steel_input():
         return target_list[num]
 
 
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -124,6 +193,18 @@ def abalone_page():
 
     return render_template('Abalone_page.html', model=age_prediction_model)
 
+
+
+@app.route('/pulsar_page', methods=['GET', 'POST'])
+def pulsar_page():
+    if request.method == 'POST':
+        return process_pulsar_input()
+    return render_template('Pulsar.html')
+
+@app.route('/pulsar_predict', methods=['POST'])
+def pulsar_predict():
+    # '/predict' 엔드포인트에서는 예측을 담당하는 process_pulsar_input() 함수를 호출하여 결과를 반환
+    return process_pulsar_input()
 
 
 
@@ -178,25 +259,12 @@ def predict():
         result = target_list[num]
     return jsonify({'prediction_result': result})
 '''
+
 # 긴 결과값이 나으는 문제 해결을 위해 predict() 클래스 변경
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/steel_predict', methods=['POST'])
+def steel_predict():
     # '/predict' 엔드포인트에서는 예측을 담당하는 process_steel_input() 함수를 호출하여 결과를 반환
     return process_steel_input()
-
-@app.route('/pulsation_page', methods=['GET', 'POST'])
-def pulsation_page():
-    global mactility_discrimination_model
-
-    if request.method == 'POST':
-        # 모델에 입력 데이터 전달하여 예측
-        # 예측 결과를 원하는 방식으로 가공하거나 다른 작업 수행
-        # ...
-
-        # 예측 결과를 화면에 출력하거나 다른 동작을 수행하는 코드 추가
-        # ...
-
-        return render_template('pulsation_page.html', model=mactility_discrimination_model)
 
 
 if __name__ == '__main__':
--- model3/inference_test.py
+++ model3/inference_test.py
@@ -9,21 +9,24 @@ model1 = joblib.load(model_path1)
 model_path2 = os.path.join(os.path.dirname(__file__), 'steelplate_model2.pkl')
 model2 = joblib.load(model_path2)
 
+model_path3 = os.path.join(os.path.dirname(__file__), 'steelplate_model3.pkl')
+model3 = joblib.load(model_path3)
+
 # 테스트 변수 지정
-X_Minimum = 77
-X_Maximum = 77
-Y_Minimum = 77
-Y_Maximum = 77
-Pixels_Areas = 77
-X_Perimeter = 77
-Y_Perimeter = 77
-Sum_of_Luminosity = 77
-Minimum_of_Luminosity = 77
-Maximum_of_Luminosity = 77
-Length_of_Conveyer = 77
-Steel_Plate_Thickness = 77
-Empty_Index = 77
-Type_of_Steel = 0
+X_Minimum = 805
+X_Maximum = 811
+Y_Minimum = 598090
+Y_Maximum = 598094
+Pixels_Areas = 16
+X_Perimeter = 6
+Y_Perimeter = 4
+Sum_of_Luminosity = 2034
+Minimum_of_Luminosity = 110
+Maximum_of_Luminosity = 140
+Length_of_Conveyer = 1360
+Steel_Plate_Thickness = 50
+Empty_Index = 0.3333
+Type_of_Steel = 1
 
 # 개별 데이터를 NumPy 배열로 표현
 individual_data = np.array([X_Minimum, X_Maximum, Y_Minimum, Y_Maximum, Pixels_Areas,
@@ -34,18 +37,29 @@ individual_data = np.array([X_Minimum, X_Maximum, Y_Minimum, Y_Maximum, Pixels_A
 # 개별 데이터를 2차원 배열로 변환
 data_array = individual_data.reshape(1, -1)
 
-# 예측 수행
+# 1차 예측 수행
 prediction1 = model1.predict(data_array)
 
 # 타겟 리스트
-target_list = ['Pastry', 'Z_Scratch', 'K_Scatch', 'Stains', 'Dirtiness', 'Bumps', 'Other_Faults']
+target_list = ['Pastry', 'Z_Scratch', 'K_Scratch', 'Stains', 'Dirtiness', 'Bumps', 'Other_Faults']
 
-# 결과 반환
-if prediction1.tolist()[0] == 5 or prediction1.tolist()[0] == 6 :
+if prediction1.tolist()[0] == 0 or prediction1.tolist()[0] == 5 or prediction1.tolist()[0] == 6 : # 예측값이 0, 5, 6인 경우
+    
+    # 2차 예측 수행
     data_array2 = np.delete(data_array, -2, axis=1)  # 'Empty Index' 데이터 삭제
-    prediction2 = model2.predict(data_array2) # 예측
-    num = prediction2.tolist()[0]
-    print(target_list[num])
-else :
+    prediction2 = model2.predict(data_array2) 
+
+    if prediction1.tolist()[1] == 0 : # 2차 예측시 1인 경우
+
+        # 3차 예측 수행
+        prediction3 = model3.predict(data_array2) # data_array2 데이터 그대로 사용
+        num = prediction2.tolist()[0]
+        print(target_list[num])
+              
+    else : # 2차 예측시 0인 경우 3차 예측 거치지 않음
+        num = prediction2.tolist()[0]
+        print(target_list[num])
+
+else : # 1차 예측시 1, 2, 3, 4인 경우 2,3차 예측 거치지 않음
     num = prediction1.tolist()[0]
     print(target_list[num])
